a59a09e9af683d58829267c413c6014343d93f1f
Daan <daan.verheij@student.rocva.nl>
2021-01-05 11:41:27 +0100

    taak 03 - strtoupper

---
+++ 6-Functions/taak03 - strtoupper()/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function telAlleWoorden(){
+    $test = "Mijn blauwe auto is een mooie snelle wagen";
+    $test = strlen($test);
+    echo $test; 
+
+            
+}
+telAlleWoorden();
+?>
+
+</body>
+</html> 
